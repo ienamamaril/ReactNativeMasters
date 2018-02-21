cf0d0507234e0648d221dfd7ff1c8f19dbadb97e
Siena Mamaril <sienam@magenic.com>
2018-02-21 17:55:31 +0800

    Assignment #2 (Week 2) - Removed KeyboardAvoidingView

--- app/modules/Login.js
+++ app/modules/Login.js
@@ -15,48 +15,44 @@ export default class Login extends React.Component {
     render () {
         const { navigation } = this.props;
         return (
-            <KeyboardAvoidingView style = {{
+            <View style = {{
                 flex: 1, 
                 flexDirection: 'column'}}>
-                {/* <View style = {{ */}
-                    {/* flex: 1, 
-                    flexDirection: 'column'}}> */}
 
-                    <View style = {{ 
-                        flex: 2, 
-                        backgroundColor: '#79BD42',
-                        alignItems: 'center', 
-                        alignContent: 'flex-start',
-                        justifyContent: 'center'}}>
-            
-                        <Text style = { styles.header }>Magenic Masters</Text>
-                        <Text style = { styles.header }>M A N I L A</Text>
-                    </View>
+                <View style = {{ 
+                    flex: 2, 
+                    backgroundColor: '#79BD42',
+                    alignItems: 'center', 
+                    alignContent: 'flex-start',
+                    justifyContent: 'center'}}>
+        
+                    <Text style = { styles.header }>Magenic Masters</Text>
+                    <Text style = { styles.header }>M A N I L A</Text>
+                </View>
 
-                    <View style = {{ 
-                        flex: 1,
-                        backgroundColor: 'black',
-                        alignItems: 'center', 
-                        alignContent: 'stretch', 
-                        justifyContent: 'center'}}>
-            
-                        <Text style = { styles.text }>Please state your </Text>
-                        
-                        <TextInput
-                            style = {{ backgroundColor: 'white', width: 150 }}
-                            placeholder = "Name"
-                            onChangeText = {( text ) => this.setState ({ text })}/>
+                <View style = {{ 
+                    flex: 1,
+                    backgroundColor: 'black',
+                    alignItems: 'center', 
+                    alignContent: 'stretch', 
+                    justifyContent: 'center'}}>
+        
+                    <Text style = { styles.text }>Please state your </Text>
+                    
+                    <TextInput
+                        style = {{ backgroundColor: 'white', width: 150 }}
+                        placeholder = "Name"
+                        onChangeText = {( text ) => this.setState ({ text })}/>
 
-                        <Button 
-                            style = { styles.button }
-                            onPress = {() => navigation.navigate('Home',{ name: this.state.text, 
-                                message: 'The answer to the ultimate question of life, the universe and everything is 42.' }
-                            )} 
-                            title = "ENTER" 
-                            color = '#79BD42'/>
-                    </View>
-                {/* </View> */}
-            </KeyboardAvoidingView>
+                    <Button 
+                        style = { styles.button }
+                        onPress = {() => navigation.navigate('Home',{ name: this.state.text, 
+                            message: 'The answer to the ultimate question of life, the universe and everything is 42.' }
+                        )} 
+                        title = "ENTER" 
+                        color = '#79BD42'/>
+                </View>
+            </View>
         )
     }
 }
