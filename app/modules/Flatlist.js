bb3d8ce8565876c2f4432bcf364d01a048ad64fe
Siena Mamaril <sienam@magenic.com>
2018-03-22 00:22:22 +0800

    Assignment #4 - FULL

--- app/modules/Flatlist.js
+++ app/modules/Flatlist.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Platform, AppRegistry, StyleSheet, View, Text, FlatList, TouchableOpacity, 
-    ScrollView, Modal, ActivityIndicator, AsyncStorage } from 'react-native';
+    ScrollView, Modal, ActivityIndicator, AsyncStorage, Button } from 'react-native';
 
 export default class Flatlist extends React.Component {
 
@@ -12,44 +12,44 @@ export default class Flatlist extends React.Component {
         }
     }
 
-    async componentWillMount () {
-        return fetch('https://launchlibrary.net/1.3/launch')
+    /*async componentDidMount () {
+        return fetch('https://launchlibrary.net/1.3/agency')
         .then ((response) => response.json())
-        .then ((responseJson) => {
-
-            // this.setState({
-            //     isLoading: false,
-            //     dataSource: responseJson.launches,
-            // }, function () {
-                try {
-                    AsyncStorage.setItem('@Key:launches', responseJson.launches);
-                 } catch (error) {
-                   // Error saving data
-                 }
-            // });
-
-
+        .then ((responseJson) => 
+        {
+            try {
+                AsyncStorage.setItem('agencies', JSON.stringify(responseJson.agencies));
+                this.showAgenciesAsync();
+            } catch (error) {
+                // Error saving data
+            }
         })
         .catch((error) => {
             console.error(error);
         })
+    }*/
+
+    async componentDidMount () {
+        try {
+            let response = await fetch ('https://launchlibrary.net/1.3/agency');
+            if (response.status === 200) {
+                let responseJson = await response.json();            
+                AsyncStorage.setItem('agencies', JSON.stringify(responseJson.agencies));
+                this.showAgenciesAsync();
+            } else {
+                this.showAgenciesAsync();
+            }            
+        } catch (error) {
+            cosole.error(error);
+        }
     }
 
-    componentDidMount () {
-        AsyncStorage.getItem('@Key:launches', (err, value) => this.setState({
+    showAgenciesAsync() {
+        AsyncStorage.getItem('agencies', (err, value) => this.setState({
             isLoading: false, 
-            dataSource: JSON.stringify(value)}
+            dataSource: JSON.parse(value)
+        }
         ))
-        this.showAgenciesAsync();
-    }
-
-    async showAgenciesAsync() {
-        let url = 'https://launchlibrary.net/1.3/agency';
-        let response = await fetch(url);
-        let agencies = await response.json();
-        this.setState  ({
-            listData: agencies
-        })
       } 
 
     closeModal () {
@@ -69,13 +69,11 @@ export default class Flatlist extends React.Component {
                 </View>
             )
         }
-       
-        //this.showAgenciesAsync(); 
 
         return (
             <View style = {{ flex: 1, flexDirection: 'column', backgroundColor: 'black', padding: 10 }}> 
                 <View style = { styles.body }>
-                        <Text style = { styles.header }>The Rocket Family</Text>
+                        <Text style = { styles.header }>The Agency Family</Text>
                         <FlatList
                             data = { this.state.dataSource } 
                             renderItem = {
@@ -93,6 +91,11 @@ export default class Flatlist extends React.Component {
                     onRequestClose = {() => this.closeModal()}>
                         <ScrollView style = {{ flex: 1, flexDirection: 'column', backgroundColor: 'black', padding: 10 }}> 
                             <View style = { styles.body }>
+                            <Button 
+                                    style = { styles.button }
+                                    onPress = {() => this.closeModal() } 
+                                    title = "C L O S E"
+                                    color = '#79BD42'/>
                                 <Text style = { styles.footer }>
                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mattis ipsum non nibh molestie maximus. Donec mollis, elit faucibus pulvinar accumsan, nibh neque posuere ligula, malesuada commodo elit neque ac lorem. Pellentesque ullamcorper dapibus sapien eu euismod. Nunc fringilla eros ut nunc porta elementum. Fusce quis libero libero. Sed luctus ac lacus et rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus.
                                     In laoreet porta iaculis. Integer id scelerisque neque. Praesent sodales, velit id laoreet laoreet, ex ex suscipit dui, a imperdiet leo justo convallis est. Pellentesque sed sodales leo, viverra rhoncus leo. Aliquam ac sagittis mi. Morbi consectetur hendrerit nisl vitae suscipit. Praesent commodo vitae diam in tincidunt. Aliquam vitae tellus eu neque condimentum efficitur id vel erat. Fusce sodales vel turpis ut malesuada. In sit amet scelerisque mauris. Ut aliquam ligula non ipsum iaculis mattis. Aenean malesuada lorem placerat eleifend ultrices.
@@ -134,5 +137,9 @@ const styles = StyleSheet.create ({
         alignItems: 'center',
         alignContent: 'flex-start',
         justifyContent: 'center',
+    },
+    button: {
+        fontWeight: 'bold',
+        fontSize: 10
     }
 });
